698e553e14f26890dd4262371396ee14ed55fe2b
surfeiro <daniel_de_burgos@hotmail.es>
2020-06-30 20:55:12 +0100

    make it better

---
+++ src/app/components/resultado-busqueda/resultado-busqueda.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+
+@Component({
+  selector: 'app-resultado-busqueda',
+  templateUrl: './resultado-busqueda.component.html'
+})
+export class ResultadoBusquedaComponent  implements OnInit {
+
+  heroes:any[] = [];
+  termino:string;
+
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService ) {
+
+  }
+
+  ngOnInit(){
+    this.activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes( params['termino'] );
+      console.log( this.heroes );
+    });
+  }
+
+}
